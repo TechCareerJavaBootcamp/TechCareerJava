354e5ed0f9b32679a75659f157e2abb54abc725a
EmineOzbek <emine.ozbek.55@gmail.com>
2024-07-21 23:13:47 +0300

    add second week codes

--- src/main/java/techCareer/week1/Sunday/Person.java
+++ src/main/java/techCareer/week1/Sunday/Person.java
@@ -1,5 +1,7 @@
 package techCareer.week1.Sunday;
 
+import java.util.Objects;
+
 public class Person {
 
     private String name;
@@ -9,6 +11,25 @@ public class Person {
     }
 
     public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return Objects.equals(getName(), person.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getName());
+    }
+
+    public Person() {
+    }
+
+    public Person(String name) {
         this.name = name;
     }
 }
